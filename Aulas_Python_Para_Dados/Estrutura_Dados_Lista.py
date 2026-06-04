017426fb703ef7e10cbd920eea69aab1ddca0470
renanrosaf <renan1995rf@gmail.com>
2026-06-04 17:19:32 -0300

    Correção README.md

--- Aulas_Python_Para_Dados/Estrutura_Dados_Lista.py
+++ Aulas_Python_Para_Dados/Estrutura_Dados_Lista.py
@@ -1,11 +1,18 @@
 #Lista: Coleção ordenada de objetos mutaveis
 
-#1:Criando uma lista: Utlizo de []
+#1:CRIANDO UMA LISTA : Utlizo de []
 telefones =['1234-5678','9999-9999','8765-4321','8877-7768']
 print(telefones)    #imprimindo a lista criada
 
-#Adicionando elementos em uma lista
-#1° Forma: Método Append,nesse método o elemento adicionado irá para a ultima
+#Lista é uma sequência de valores, espécie de container de valores do mesmo tipo ou diferentes tipos
+
+#2:ACESSANDO ELEMENTOS DE UMA LISTA
+#indico indice no colchetes
+#1°Elemento de uma lista: indice 0
+print(telefones[0])
+
+#3: ADICIONANDO ELEMENTOS EM UMA LISTA
+#1° Forma: Método Append,nesse método o elemento adicionado irá para a ultima poisção da lista
 #posição da lista criada e só utilizo o parametro do elemento a ser adicionado
 telefones.append('555-5555')
 print(telefones)
@@ -13,11 +20,11 @@ print(telefones)
 #2° Forma: Método Insert
 #Nesse método eu preciso indicar a posição da lista de onde eu quero 
 #inserir o objeto a ser adcionado, além do elemento 
-#indiquei a posição 0
+#indiquei a posição 0, ou seja sera na primeira posição da lista
 telefones.insert(0,"444-44444")
 print(telefones)
 
-#2:Removendo elementos de uma lista
+#4 REMOVENDO ELEMENTOS DE UMA LISTA:
 #1: Forma: Usar o método remove
 #Nesse método, parametros é apenas o elemento que quero remover
 telefones.remove("444-44444")
@@ -29,25 +36,3 @@ print(telefones)
 telefones.pop(1)
 print(telefones)
 
-#3° Tupla({});  Crio usando {}
-contato=("Yan","324-5678") #contato não esta dentro uma lista
-
-#adicionando uma tupla em uma lista
-telefones2=[] #lista vazia de elementos, primeiro crio lista vazia
-telefones2.append(contato) #depois uso append.(nomedatupla)
-print (telefones2)
-
-#outra forma: Crio uma lista de tuplas, items com mais de um valor 
-#Lista com tres tuplas
-telefones3=[("Yan","2343-6678"),("Ana","3345-6678"),("Carlos","3345-6679")]
-print(telefones3)
-
-#Acessando elemento de uma lista de tuplas: Aqui não posso acessar com o indice
-#pois cada item tem 2 posições
-print(telefones3[0])
-
-#imprimindo apenas o numero do Yan'
-print(telefones3[0][1])
-
-#imprimindo apenas o numero da ana
-print(telefones3[1][1])
--- Aulas_Python_Para_Dados/Estrutura_Dados_Tupla.py
+++ Aulas_Python_Para_Dados/Estrutura_Dados_Tupla.py
@@ -1,24 +1,29 @@
 
+#TUPLA: ESTRUTURA QUE APRESENTA 2 VALORES (POSIÇÕES)
 #CRIANDO UMA TUPLA: CRIO USANDO ()
 contato=("Yan","324-5678") #contato não esta dentro uma lista ainda  e sim uma tupla
 
 #ADICIONANDO  UMA TUPLA EM UMA LISTA
-#Forma1:
+
+#Forma1:Crio uma tupla e adficiono em uma lista
 telefones2=[] #lista vazia de elementos, primeiro crio lista vazia
 telefones2.append(contato) #depois uso append.(nomedatupla)
 print (telefones2)
 
 #Forma 2:
 #outra forma: Crio uma lista de tuplas, items com mais de um valor 
-#Lista com tres tuplas
+
+#Lista com tres tuplas: Items com mais de uma valor 
 telefones3=[("Yan","2343-6678"),("Ana","3345-6678"),("Carlos","3345-6679")]
 print(telefones3)
+
+#ACESSAR ELEMENTO DE UMA LISTA:
 
 #Acessando elemento de uma lista de tuplas: Aqui não posso acessar com o indice
 #pois cada item tem 2 posições
 print(telefones3[0])
 
-#imprimindo apenas o numero do Yan'
+#imprimindo apenas o numero do Yan' #POSIÇÃO O DA LISTA (DADOS YAN), POSIÇÃO 1: YAN
 print(telefones3[0][1])
 
 #imprimindo apenas o numero da ana
